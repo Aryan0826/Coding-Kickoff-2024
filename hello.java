8da314d4cad8518ed6f8ebdacb3c92555eb695a1
Aryan0826 <vekariyaaryan100@gmail.com>
2024-09-27 17:47:55 -0500

    Added Hello.java and readme

---
+++ hello.java
@@ -0,0 +1,17 @@
+public class hello{
+    public static void main(String[] args) {
+        System.out.println("hello");
+        //weve jus created 2 files  tht r not backed up yet
+        //git add filename tracks and backups the file
+        // instead of adding one by  one .. you can jus do "git add ." . is the current position
+        // it adds all the files at the current position
+
+
+        // to confirm the backup... git commit -m "describe wht u did"
+        //switchinng to a new branch git checkout -b branchname
+
+
+        //removing a repository  .... launch gitbash rm -rf .git
+        //then git init
+    }
+}
